c021c6da3c02d47c67c4b12630e538b62163f262
jldaggs88 <jessalyn.daggs@gmail.com>
2020-10-12 03:29:02 -0500

    Code clean up

--- app.js
+++ app.js
@@ -27,15 +27,74 @@ const container = database.container(containerId);
 async function main(){
 	await dbContext.create(cosClient, databaseId, containerId);
 	console.log(`Querying container: ${container.id}`);
+	
+	client.addListener('message', async (from, to, text)=>{
+		let str = text;
+		if(str.indexOf('BUGREPORT') === -1){
+			if(str.indexOf('BUG #') === -1){
+				client.say('#BugsChannel', "Oops, I couldn't process your request.\n Send me commands like,\n BUG # <BUG. NO>");
+			}
+			else {
+				client.say('#BugsChannel', `So you need info about "${text}`);
+				let t= text.substring(5,text.length);
+				let temp = t.trim();
 
-	// query to return all items
-	const querySpec = {
-		query: "SELECT * from c"
-	};
+				let queryById = {
+					query: 'SELECT * FROM Bugs b WHERE b.id= @id',
+					parameters: [{
+						name: '@id',
+						value: temp
+					}]
+				};
 
-	const { resources: items } = await container.items
-  .query(querySpec)
-	.fetchAll();
+				({ resources: queryById } = await container.items
+				.query(queryById)
+				.fetchAll());
+				
+				if(queryById.length > 0){
+					client.say('#BugsChannel', `${queryById[0].url} ~ Status: ${queryById[0].status} Title: ${queryById[0].title}`);
+				} else {
+					client.say('#BugsChannel', 'No bugs found.');
+				}
+			}
+		} else {
+			client.say('#BugsChannel', "Working on your Bug Report!");
+			client.say('#BugsChannel', "One Sec!");
+
+			let queryOpenItems = {
+				query: 'SELECT * FROM Bugs b WHERE b.status= @status',
+				parameters: [{
+					name: '@status',
+					value: 'Open'
+				}]
+			};
+
+			({ resources: queryOpenItems } = await container.items
+			.query(queryOpenItems)
+			.fetchAll());
+
+			console.log(queryOpenItems.length)
+			client.say('#BugsChannel', `Open Bugs: ${queryOpenItems.length}`);
+
+			let queryClosedItems = {
+				query: 'SELECT * FROM Bugs b WHERE b.status= @status',
+				parameters: [{
+					name: '@status',
+					value: 'Closed'
+				}]
+			};
+
+			({ resources: queryClosedItems } = await container.items
+			.query(queryClosedItems)
+			.fetchAll());
+	
+			client.say('#BugsChannel', `Closed Bugs: ${queryClosedItems.length}`);
+		}
+		
+	})
+	client.on('error', (err) => {
+		console.warn(err.message)
+ });
 }
 
 async function handleError(error) {
@@ -44,94 +103,3 @@ async function handleError(error) {
 }
 
 main().catch(handleError);
-// const cosClient = new CosmosClient("AccountEndpoint=https://bugbotdb.documents.azure.com:443/;AccountKey=1Yx7lotfapV9lwF28S0zlSCBXUgxKqzhWABF1opviIcuxJX1eI6wSI9HqD7iIRlnEEGHj4dR71cNfiBqXjMY6A==;")
-// async function main() {
-
-// 	const database = cosClient.database(databaseId);
-// 	const container = database.container(containerId);
-
-// 	console.log('container', container.database.client);
-	
-// 	// Make sure Tasks database is already setup. If not, create it.
-// 	// await dbContext.create(cosClient, databaseId, containerId);
-// 	// try {
-// 		// console.log(`Querying container: ${containerId}`);
-// 		// const { resource: createdItem } = await container.items.create(newItem);
-// 		// console.log(`\r\nCreated new item: ${createdItem.id} - ${createdItem.description}\r\n`);
-
-// 		client.addListener('message', (from, to, text)=>{
-// 			console.info("made it to the even listener");
-// 			let str = text;
-// 			if(str.indexOf('BUGREPORT') === -1){
-// 				if(str.indexOf('BUG #') === -1){
-// 					client.say('#BugsChannel', "I could not get that!\n Send me commands like,\n BUG # <BUG. NO>");
-// 				}
-// 				else {
-// 					client.say('#BugsChannel', `So you need info about "${text}`);
-// 					client.say('#BugsChannel', "Wait for a moment!");
-// 					let t= text.substring(6,text.length);
-// 					let temp = t.trim();
-					
-// 					let querySpec = {
-// 						query: 'SELECT * FROM Bugs b WHERE b.id= @id',
-// 						parameters: [{
-// 							name: '@id',
-// 							value: temp
-// 						}]
-// 					};
-
-// 					// read all items in the container by the given value
-// 					const { resources: bugsById } = container.items
-// 					.query(querySpec)
-// 					.fetchAll();
-		
-// 					if(BugsById.length > 0){
-// 						client.say('#BugsChannel', `[${bugsById[0].url}] [Status]: ${bugsById[0].status} [Title]: ${bugsById[0].title}`);
-// 					}
-// 					else {
-// 						client.say('#BugsChannel', 'No bugs found.');
-// 					}
-// 				}
-// 			}
-// 			else {
-// 				client.say('#BugChannel', "So you need a Bug Report!");
-// 				client.say('#BugsChannel', "Wait for a moment!");
-// 				let querySpec2 = {
-// 					query: 'SELECT * FROM Bugs b WHERE b.status= @status',
-// 					parameters: [{
-// 						name: '@status',
-// 						value: 'Open'
-// 					}]
-// 				};
-
-// 				// read all items in the container with open status
-// 				const { resources: openItems } = container.items
-// 				.query(querySpec2)
-// 				.fetchAll();
-		
-// 				client.say('#BugsChannel', `Total Open Bugs: ${openItems.length}`);
-		
-// 				let querySpec3 = {
-// 					query: 'SELECT * FROM Bugs b WHERE b.status= @status',
-// 					parameters: [{
-// 						name: '@status',
-// 						value: 'Closed'
-// 					}]
-// 				};
-		
-// 				// read all items in the container with closed status
-// 				const { resources: closedItems } = container.items
-// 				.query(querySpec3)
-// 				.fetchAll();
-		
-// 				client.say('#BugsChannel', `Total Closed Bugs: ${closedItems.length}`);
-// 			}
-// 		});
-// }
-
-// async function handleError(error) {
-//     console.log("\nAn error with code '" + error.code + "' has occurred:");
-//     console.log("\t" + error.body || error);
-// }
-
-// main().catch(handleError);
